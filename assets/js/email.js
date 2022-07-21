753f610437674b4058c642093c71d92c960a9555
sergiowgriffa <sergiowgriffa@gmail.com>
2022-07-21 18:41:08 -0300

    Agregando Librerias de JS

---
+++ assets/js/email.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById('button');
+document.getElementById('form')
+  .addEventListener('submit', function(event) {
+    event.preventDefault();
+    btn.value = 'Enviando...';
+    const serviceID = 'default_service';
+    const templateID = 'template_llcgaqk';
+    emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Enviar Email';
+      alert(JSON.stringify(err));
+    });
+});
